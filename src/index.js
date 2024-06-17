ac04ff913359c74ca8e45ec8fe1d0cd34f097d5b
_Dakshin_ <147589623+2Duckduck@users.noreply.github.com>
2024-06-17 05:56:06 +0000

    second commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-//document.getElementById("open").innerText = "6"
-
-let countel = document.getElementById("open")
-
-let count = 0
-function increment(){
-    count = count + 1
-    countel.innerText = count
-}
